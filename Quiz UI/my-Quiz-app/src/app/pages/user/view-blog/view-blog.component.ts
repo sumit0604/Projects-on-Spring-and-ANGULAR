c36c04e8a46703f53b08f34fca1c01475db9104b
Joshi <sumit.b.joshi@capgemini.com>
2022-03-25 22:20:48 +0530

    fifth commit

--- Quiz UI/my-Quiz-app/src/app/pages/user/view-blog/view-blog.component.ts
+++ Quiz UI/my-Quiz-app/src/app/pages/user/view-blog/view-blog.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-blog.component.css']
 })
 export class ViewBlogComponent implements OnInit {
+i=0;
+
+values:any
+// like_no:any
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+  clicked(){
+    // this.like =true;
+    // this.i=this.i+1;
+    this.setlike()
+    this.getlikess()
+    console.log(this.getlikess())
+  }
+  setlike(){
+    // this.i=this.i+1;
+    this.values = this.i+1
+    localStorage.setItem('likesss', this.values)
+    this.values=this.values+1
+  }
+  getlikess() {
+    return localStorage.getItem('likesss')
+ }
 
 }
